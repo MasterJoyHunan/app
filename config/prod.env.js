a5199424830c1b892ee52ff28d4f96e392124be0
Master Joy <386442876@qq.com>
2021-05-13 15:46:36 +0800

    添加k8s支持

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,8 +1,6 @@
 "use strict"
 module.exports = {
     NODE_ENV: '"production"',
-    CDN: '"http://www.masterjoy.top/uploads/"',
-    BASE_API: '"http://www.masterjoy.top"',
-    // CDN: '"http://localhost/web/public/uploads/"',
-    // BASE_API: '"http://localhost/web/public"'
+    CDN: '"/uploads/"',
+    BASE_API: '"/"',
 }
